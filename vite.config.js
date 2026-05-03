3b31fe33461f5b259f40f915a33b2f5bebbdf33c
Brunoverly77 <Brunoverlysantos7@gmail.com>
2026-05-02 22:00:40 -0300

    fix: ajuste de base para compatibilidade Vercel e Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? '/Portfolio/' : '/',
-}
+})
